43d41bb24a0f5ef4a44d4cbb13540909a24c4c73
H1yori233 <hiyoriizumi233@gmail.com>
2026-02-14 13:30:09 -0800

    fix

--- fastvideo/models/dits/matrixgame/action_module.py
+++ fastvideo/models/dits/matrixgame/action_module.py
@@ -751,7 +751,7 @@ class ActionModule(nn.Module):
                 device=target_device, dtype=target_dtype
             )
         # else:
-            return x
+            # return x
 
         B, N_frames, C = keyboard_condition.shape
         assert tt * th * tw == x.shape[1]
